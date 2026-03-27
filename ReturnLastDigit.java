0670bc9229372066d20dd43c3b63d230f8d92da0
tejalkv0 <tejalkolambe6@gmail.com>
2026-03-27 12:27:02 +0530

    updated logic

--- ReturnLastDigit.java
+++ ReturnLastDigit.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 
 public class ReturnLastDigit {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter a number");
-        int num=sc.nextInt();
-        //int lastDigit=num%10;
-        int firstDigit=num/10;
-       
-        System.out.println("firstDigit is:"+firstDigit);
+        int num = sc.nextInt();
+        // int lastDigit=num%10;
+
+        while (num >= 10) {
+
+            num = num / 10;
+
+        }
+        System.out.println("first digit:" + num);
+
     }
-    
+
 }
